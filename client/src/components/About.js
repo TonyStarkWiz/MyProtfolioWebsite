b8159ef10e3cff1fe04e4d34b9fb87d360f654e8
TonyStarkWiz <anthonyespinosa33891@gmail.com>
2025-03-22 09:57:18 -0400

    Update About section to 9 years of experience

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const About = () => (
   <div>
-    <p>Back in 2013, I embarked on a journey to develop Android apps, diving deep into the realm of software engineering and web development. Fast forward to today, I have over 8 years of experience specializing in Android development.</p>
+    <p>Back in 2013, I embarked on a journey to develop Android apps, diving deep into the realm of software engineering and web development. Fast forward to today, I have over 9 years of experience specializing in Android development.</p>
     <p>My primary focus now is on enhancing user experiences through innovative and efficient solutions.</p>
     <p>In addition to my development expertise, I have a solid foundation in mathematics and AI. With extensive experience in 17 branches of mathematics, I have demonstrated exceptional problem-solving skills.</p>
     <p>When I'm not coding, I indulge in financial trading, applying my analytical skills to manage virtual portfolios successfully. As a dedicated karateka, I embody discipline and perseverance.</p>
